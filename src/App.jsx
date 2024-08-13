8854367f301e308c2f082f4e7fc1e87f51e63eef
Priyanshi <priyanshikasaudhan8429@gmail.com>
2024-08-14 01:18:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import { useState, useCallback, useEffect, useRef } from "react";
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numAllow, setNumAllow] = useState(false);
+  const [charAllow, setCharAllow] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //useRef hook
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numAllow) str += "0123456789";
+    if (charAllow) str += "!@#$%^&*(){}[]~`-=+";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numAllow, charAllow, setPassword]);
+
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0, 100)
+
+    window.navigator.Clipboard.writeText(password)
+  }, [password])
+
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numAllow, charAllow, passwordGenerator]);
+
+  return (
+    <>
+      <div className="w-full max-w-md  shadow-md rounded-lg px-10  py-10 mx-96 text-orange-500    bg-gradient-to-r from-violet-500 to-fuchsia-500">
+        <h3 className="text-white text-center my-3 text-2xl">Password Generator</h3>
+        {/* password div */}
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button 
+          onClick={copyPasswordToClipboard}
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          {/* Length div */}
+          <div className="flex item-center gap-x-1 text-white">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length:{length}</label>
+          </div>
+          {/* Number div */}
+          <div className="flex item-center gap-x-1 text-white">
+            <input
+              type="checkbox"
+              defaultChecked={numAllow}
+              id="numberInput"
+              onChange={() => {
+                setNumAllow((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Number</label>
+          </div>
+          {/* Charater Div */}
+          <div className="flex item-center gap-x-1 text-white">
+            <input
+              type="checkbox"
+              defaultChecked={charAllow}
+              id="charInput"
+              onChange={() => {
+                setCharAllow((prev) => !prev);
+              }}
+            />
+            <label htmlFor="charInput">Charater</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
